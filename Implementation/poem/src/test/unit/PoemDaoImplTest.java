7b18a1cd318f17a93555a0b65cf9da8fbc5be76a
Kerie <zkkzw2008@gmail.com>
2012-12-22 03:14:24 +0800

    changed junit test import library.

--- Implementation/poem/src/test/unit/PoemDaoImplTest.java
+++ Implementation/poem/src/test/unit/PoemDaoImplTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Assert;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Before;
@@ -15,6 +13,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import dao.PoemDao;
+import domain.Favorite;
 import domain.Poem;
 
 public class PoemDaoImplTest {
@@ -36,13 +35,18 @@ public class PoemDaoImplTest {
 		poem.setPid(300);
 		poem = poemDao.findById(poem);
 		log.info("poem find by id title is : " + poem.getTitle());
+<<<<<<< HEAD
 		Assert.assertNotNull(poem);
 		Assert.assertEquals("ï¿½ï¿½ï¿½Ò°ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ç¾°ï¿½ï¿½", poem.getTitle());
+=======
+		assertNotNull(poem);
+		assertEquals("Óæ¼Ò°Á¡¤ÈûÏÂÇïÀ´·ç¾°Òì", poem.getTitle());
+>>>>>>> changed junit test import library.
 
 		poem.setPid(3000);
 		log.info("find a non existing id : 3000");
 		poem = poemDao.findById(poem);
-		Assert.assertNull(poem);
+		assertNull(poem);
 	}
 
 	@Test
@@ -56,29 +60,65 @@ public class PoemDaoImplTest {
 	}
 
 	@Test
-	public void testQueryAll() {
-		List<Poem> list = new ArrayList<Poem>();
-		// we need to compare ids only, because objects have association relationships. -_-!!!
-		List<Integer> ids = new ArrayList<Integer>();
-		list = poemDao.queryAll();
-		for (Poem poem : list) {
-			ids.add(poem.getPid());
-			log.info("poem title in all poems list: " + poem.getTitle());
-		}
-		Assert.assertNotNull(list);
-		for (int i = 0; i < 6; i++) {
-			Assert.assertTrue(ids.contains(300 + i));
-		}
-	}
-
-	@Test
 	public void testUpdate() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	public void testQueryAll() {
+		List<Poem> list = new ArrayList<Poem>();
+		// we need to compare ids only, because objects have association
+		// relationships. -_-!!!
+		List<Integer> ids = new ArrayList<Integer>();
+		list = poemDao.queryAll();
+		for (Poem poem : list) {
+			log.info("poem id in query all list: " + poem.getPid());
+			ids.add(poem.getPid());
+			log.info("poem title in all poems list: " + poem.getTitle());
+		}
+		assertNotNull(list);
+		for (int i = 0; i < 6; i++) {
+			assertTrue(ids.contains(300 + i));
+		}
+	}
+
+	@Test
 	public void testAddToFavorites() {
-		fail("Not yet implemented");
+		Poem poem = new Poem();
+		poem.setPid(300);
+		poem = poemDao.findById(poem);
+		log.info("poem add to favorites: " + poem.getPid());
+
+		List<Favorite> oldfl = new ArrayList<Favorite>(poem.getFavoriteLists());
+		assertNotNull(oldfl);
+		for (Favorite favorite : oldfl) {
+			log.info(poem.getTitle() + " already in list: " + favorite.getFid());
+		}
+
+		List<Favorite> newfl = new ArrayList<Favorite>();
+		for (int i = 0; i < 3; i++) {
+			Favorite f = new Favorite();
+			f.setFid(300 + i);
+			log.info("favorite id in newfl: " + f.getFid());
+			newfl.add(f);
+		}
+
+		poemDao.addToFavorites(newfl, poem);
+		poem = poemDao.findById(poem);
+		newfl = new ArrayList<Favorite>(poem.getFavoriteLists());
+		assertNotNull(newfl);
+
+		List<Integer> ids = new ArrayList<Integer>();
+		for (Favorite f : newfl) {
+			log.info("favorite id in newfl: " + f.getFid());
+			ids.add(f.getFid());
+		}
+
+		for (int i = 0; i < 3; i++) {
+			assertTrue(ids.contains(300 + i));
+		}
+
+		poemDao.addToFavorites(oldfl, poem);
 	}
 
 	@Test
--- Implementation/poem/src/test/unit/UserCountDaoImplTest.java
+++ Implementation/poem/src/test/unit/UserCountDaoImplTest.java
@@ -1,6 +1,6 @@
 package test.unit;
 
-import junit.framework.Assert;
+import static org.junit.Assert.*;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -30,12 +30,12 @@ public class UserCountDaoImplTest {
 	public void testFindById() {
 		UserCount user = userCountDao.findById(301);
 		log.info("user name find by dao : " + user.getName());
-		Assert.assertNotNull(user);
-		Assert.assertEquals("keke", user.getName());
+		assertNotNull(user);
+		assertEquals("keke", user.getName());
 
 		log.info("find a non existing id : 3000");
 		user = userCountDao.findById(3000);
-		Assert.assertNull(user);
+		assertNull(user);
 	}
 
 	@Test
@@ -46,9 +46,9 @@ public class UserCountDaoImplTest {
 		UserCount user2 = userCountDao.checkLogin(user1);
 		log.info("check login user name: " + user2.getName());
 		log.info("check login user password: " + user2.getPassword());
-		Assert.assertNotNull(user2);
-		Assert.assertEquals(user1.getName(), user2.getName());
-		Assert.assertEquals(user1.getPassword(), user2.getPassword());
+		assertNotNull(user2);
+		assertEquals(user1.getName(), user2.getName());
+		assertEquals(user1.getPassword(), user2.getPassword());
 	}
 
 	@Test
@@ -58,7 +58,7 @@ public class UserCountDaoImplTest {
 		user1.setPassword("123456");
 		log.info("non existing user login with name iamNotExisting, password 123456");
 		UserCount user2 = userCountDao.checkLogin(user1);
-		Assert.assertNull(user2);
+		assertNull(user2);
 	}
 
 	@Test
@@ -67,7 +67,7 @@ public class UserCountDaoImplTest {
 		user1.setName("keke");
 		log.info("an existing user check with name " + user1.getName());
 		UserCount user2 = userCountDao.checkUserName(user1);
-		Assert.assertNotNull(user2);
+		assertNotNull(user2);
 	}
 
 	@Test
@@ -76,7 +76,7 @@ public class UserCountDaoImplTest {
 		user1.setName("iamNotExist");
 		log.info("non existing user check with name " + user1.getName());
 		UserCount user2 = userCountDao.checkUserName(user1);
-		Assert.assertNull(user2);
+		assertNull(user2);
 	}
 
 	@Test
@@ -85,7 +85,7 @@ public class UserCountDaoImplTest {
 		user1.setEmail("keke@poem.com");
 		log.info("an existing user check with email " + user1.getEmail());
 		UserCount user2 = userCountDao.checkUserEmail(user1);
-		Assert.assertNotNull(user2);
+		assertNotNull(user2);
 	}
 
 	@Test
@@ -94,7 +94,7 @@ public class UserCountDaoImplTest {
 		user1.setEmail("iamNotExist@notexisting.com");
 		log.info("non existing user check with email " + user1.getEmail());
 		UserCount user2 = userCountDao.checkUserEmail(user1);
-		Assert.assertNull(user2);
+		assertNull(user2);
 	}
 
 	@Test
@@ -105,36 +105,36 @@ public class UserCountDaoImplTest {
 		log.info("test user register with name: " + user.getName()
 				+ "and password: " + user.getPassword());
 		boolean result = userCountDao.regist(user);
-		Assert.assertEquals(result, true);
+		assertEquals(result, true);
 
 		log.info("registered user should be able to login");
 		user = userCountDao.checkLogin(user);
-		Assert.assertNotNull(user);
+		assertNotNull(user);
 
 		log.info("we should delete test data after adding");
 		userCountDao.delete(user);
 
 		log.info("deleted user should not be able to login");
 		user = userCountDao.checkLogin(user);
-		Assert.assertNull(user);
+		assertNull(user);
 	}
 
 	@Test
 	public void testUpdateUserCount() {
 		UserCount user = userCountDao.findById(301);
-		Assert.assertEquals("keke", user.getName());
+		assertEquals("keke", user.getName());
 		log.info("before we update, user name is: " + user.getName());
 		user.setName("kerie");
 		userCountDao.updateUserCount(user);
 
 		user = userCountDao.findById(301);
 		log.info("after we update, user name is: " + user.getName());
-		Assert.assertEquals("kerie", user.getName());
+		assertEquals("kerie", user.getName());
 
 		log.info("we should set the name back after testing, or we will fail the test next time");
 		user.setName("keke");
 		userCountDao.updateUserCount(user);
-		Assert.assertEquals("keke", user.getName());
+		assertEquals("keke", user.getName());
 	}
 
 }
